56fcec807bd99cc7b4795a8749a97904b0343e51
huupuhoc <huuphuoc622004@gmail.com>
2025-12-17 02:01:58 +0700

    Chỉnh sửa Title

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h1>Báo cáo Bài tập 1: React trên Vercel</h1>
+      <h2>Trần Hữu Phước</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
